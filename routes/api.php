0867587b437f574ab282bb483a0c14f45bb74e86
antspring <max.verzilov32@gmail.com>
2025-05-21 23:45:30 +0500

    chore: transfer routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\IngredientController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('/v1/')->group(function () {
+    Route::apiResource('category', CategoryController::class);
+    Route::apiResource('ingredient', IngredientController::class);
+    Route::apiResource('product', ProductController::class);
+});
